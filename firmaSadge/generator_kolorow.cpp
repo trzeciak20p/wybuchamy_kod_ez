3022dfa41fd1bb276392a60c5bbcdb8ca99b0cd2
wiora20p <91263424+wiora20p@users.noreply.github.com>
2022-03-28 09:48:27 +0200

    Update generator_kolorow.cpp

--- firmaSadge/generator_kolorow.cpp
+++ firmaSadge/generator_kolorow.cpp
@@ -5,12 +5,35 @@ using namespace std;
 
 int main(int argc, char const *argv[]){
 
-    string kolory[4] = ["czarny", "żółty", "różowy", "niebiesko-biały"];
+    string kolory[4] = {"czarny", "żółty", "różowy", "niebiesko-biały"};
     int ile = 200;
     for(int i = 0; i < ile; i++){
-        cout << kolory[rand() % 3]
+        cout << kolory[rand() % 4]
     }
     
     return 0;
 }
 
+// losowanie niepowtarzalnych liczb
+
+int main(int argc, char const *argv[]){
+
+    string tab[4] = {"spalinowy", "elektryczny", "hybryda", "diesel"};
+    bool t[150];
+    int ile = 200;
+
+    for(int i = 0; i < 150; i++){
+        t[i] = false;
+    }
+
+    for(int i = 0; i < ile; i++){
+        int x = rand() % 150;
+        while(t[x]){
+            x = rand() % 150;
+        }
+        cout << x << endl;
+        t[x] = true;
+    }
+    
+    return 0;
+}
